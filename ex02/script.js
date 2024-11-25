ef4c1faf422ec004e2655b64334bb047641a550a
Chloe Lingle <firelilly0200@gmail.com>
2024-11-25 11:33:43 -0500

    document. getElementById(myDiv). style. backgroundColor = red

--- ex02/script.js
+++ ex02/script.js
@@ -9,6 +9,7 @@ function task1() {
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  document. getElementById(myDiv). style. backgroundColor = red
 }
 
 // Write your task 3 code inside this function!
